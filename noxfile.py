acd19529e1d0efd23b7d8a98a5fd080d7901b931
s33zganji <s33zganj@uni-bonn.de>
2025-10-08 10:23:26 +0200

    noxfile updated

--- noxfile.py
+++ noxfile.py
@@ -10,22 +10,6 @@ def run_test(session):
     session.run("pytest")
 
 
-@nox.session(name="lint")
-def lint(session):
-    """Check code conventions."""
-    session.install("flake8==4.0.1")
-    session.install(
-        "flake8-colors",
-        "flake8-black",
-        "flake8-docstrings",
-        "flake8-bugbear",
-        "flake8-broken-line",
-        "pep8-naming",
-        "pydocstyle",
-        "darglint",
-    )
-    session.install("flake8-bandit==2.1.2", "bandit==1.7.2")
-    session.run("flake8", "src", "tests", "noxfile.py")
 
 
 @nox.session(name="typing")
@@ -35,21 +19,7 @@ def mypy(session):
     session.install("mypy")
     session.run(
         "mypy",
-        "--install-types",
-        "--non-interactive",
-        "--ignore-missing-imports",
-        "--no-strict-optional",
-        "--no-warn-return-any",
-        "--implicit-reexport",
-        "--allow-untyped-calls",
         "src",
     )
 
 
-@nox.session(name="format")
-def format(session):
-    """Fix common convention problems automatically."""
-    session.install("black")
-    session.install("isort")
-    session.run("isort", "src", "tests", "noxfile.py")
-    session.run("black", "src", "tests", "noxfile.py")
